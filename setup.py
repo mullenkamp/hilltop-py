219089184ed86d2658fbc9f129e1e1f2092d5e57
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2018-03-18 08:27:51 +1300

    test update

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='hilltoppy',  # Required
+    name='hilltop-py',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.4',  # Required
+    version='1.0.5',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -61,7 +61,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/mullenkamp/hilltoppy',  # Optional
+    url='https://github.com/mullenkamp/hilltop-py',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
