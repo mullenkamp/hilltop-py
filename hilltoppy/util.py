1cbf39566e9985a01045e59feb2746c387d0e476
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2018-03-21 09:34:25 +1300

    fixed dependencies

--- hilltoppy/util.py
+++ hilltoppy/util.py
@@ -2,7 +2,9 @@
 """
 Utility functions for Hilltop functions.
 """
-from os import path
+import os
+import numpy as np
+import pandas as pd
 from datetime import datetime
 try:
     from configparser import ConfigParser
@@ -24,7 +26,7 @@ def parse_dsn(dsn_path):
     List of path strings to hts files.
     """
 
-    base_path = path.dirname(dsn_path)
+    base_path = os.path.dirname(dsn_path)
 
     dsn = ConfigParser()
     dsn.read(dsn_path)
@@ -33,10 +35,10 @@ def parse_dsn(dsn_path):
     dsn1 = [i for i in files1 if i.endswith('.dsn')]
     while dsn1:
         for f in dsn1:
-            base_path = path.dirname(f)
+            base_path = os.path.dirname(f)
             p1 = ConfigParser()
             p1.read(f)
-            files1 = [path.join(base_path, i[1]) for i in p1.items('Hilltop') if 'file' in i[0]]
+            files1 = [os.path.join(base_path, i[1]) for i in p1.items('Hilltop') if 'file' in i[0]]
             hts1.extend([i for i in files1 if i.endswith('.hts')])
             dsn1.remove(f)
             dsn1[0:0] = [i for i in files1 if i.endswith('.dsn')]
@@ -92,13 +94,13 @@ def convert_site_names(names, rem_m=True):
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
-        names2[~names_len_bool] = nan
+        names2[~names_len_bool] = np.nan
     else:
         list_names1 = names1.str.findall('[A-Z]+\d\d/\d\d\d\d\s*-\s*M\d*')
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
-        names2[~names_len_bool] = nan
+        names2[~names_len_bool] = np.nan
 
     return names2
 
@@ -119,7 +121,7 @@ def proc_ht_use_data(ht_data, n_std=4):
 
         ### Select the process sequence based on the mtype and convert to period volume
 
-        data[data < 0] = nan
+        data[data < 0] = np.nan
 
         if mtype == 'Water Meter':
             ## Check to determine whether it is cumulative or period volume
@@ -129,21 +131,21 @@ def proc_ht_use_data(ht_data, n_std=4):
             neg_ratio = sum(neg_index.values)/count1
             if neg_ratio > 0.1:
                 outliers = abs(data - data.mean()) > (data.std() * n_std)
-                data[outliers] = nan
+                data[outliers] = np.nan
                 vol = data
             else:
                 # Replace the negative values with zero and the very large values
                 diff1[diff1 < 0] = data[diff1 < 0]
                 outliers = abs(diff1 - diff1.mean()) > (diff1.std() * n_std)
-                diff1.loc[outliers] = nan
+                diff1.loc[outliers] = np.nan
                 vol = diff1
         elif mtype in ['Compliance Volume', 'Volume']:
             outliers = abs(data - data.mean()) > (data.std() * n_std)
-            data.loc[outliers] = nan
+            data.loc[outliers] = np.nan
             vol = data
         elif mtype == 'Flow':
             outliers = abs(data - data.mean()) > (data.std() * n_std)
-            data.loc[outliers] = nan
+            data.loc[outliers] = np.nan
 
 #            # Determine the diff index
 #            t1 = Series(data.index).diff().dt.seconds.shift(-1)
@@ -154,13 +156,13 @@ def proc_ht_use_data(ht_data, n_std=4):
             vol = (data * 60*60*24).fillna(method='ffill').round(4)
         elif mtype == 'Average Flow':
             outliers = abs(data - data.mean()) > (data.std() * n_std)
-            data.loc[outliers] = nan
+            data.loc[outliers] = np.nan
             vol = (data * 24).fillna(method='ffill').round(4)
 
         res1.append(vol)
 
     ### Convert to dataframe
-    df1 = concat(res1).reset_index()
+    df1 = pd.concat(res1).reset_index()
 
     ### Drop the mtypes level and uppercase the sites
     df2 = df1.drop('mtype', axis=1)
