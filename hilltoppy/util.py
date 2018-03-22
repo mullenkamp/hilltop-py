6a7287b7350920045b633a2d18e52654ecd859d0
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2018-03-22 18:35:18 +1300

    update

--- hilltoppy/util.py
+++ hilltoppy/util.py
@@ -85,18 +85,18 @@ def convert_site_names(names, rem_m=True):
 
     names1 = names.str.replace('[:\.]', '/')
 #    names1.loc[names1 == 'L35183/580-M1'] = 'L35/183/580-M1' What to do with this one?
-    names1.loc[names1 == 'L370557-M1'] = 'L37/0557-M1'
-    names1.loc[names1 == 'L370557-M72'] = 'L37/0557-M72'
-    names1.loc[names1 == 'BENNETT K38/0190-M1'] = 'K38/0190-M1'
+#    names1.loc[names1 == 'L370557-M1'] = 'L37/0557-M1'
+#    names1.loc[names1 == 'L370557-M72'] = 'L37/0557-M72'
+#    names1.loc[names1 == 'BENNETT K38/0190-M1'] = 'K38/0190-M1'
     names1 = names1.str.upper()
     if rem_m:
-        list_names1 = names1.str.findall('[A-Z]+\d\d/\d\d\d\d')
+        list_names1 = names1.str.findall('[A-Z]+\d+/\d+')
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
         names2[~names_len_bool] = np.nan
     else:
-        list_names1 = names1.str.findall('[A-Z]+\d\d/\d\d\d\d\s*-\s*M\d*')
+        list_names1 = names1.str.findall('[A-Z]+\d+/\d+\s*-\s*M\d*')
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
