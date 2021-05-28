c360e6533cf7b97b5ff58c3a5e4e9b2433f27646
mullenkamp <mullenkamp1@gmail.com>
2021-05-28 16:27:29 +1200

    yet another fix...

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 name = 'hilltop-py'
 main_package = 'hilltoppy'
 # datasets = 'datasets'
-version = '1.4.11'
+version = '1.4.12'
 descrip = 'Functions to access Hilltop data'
 
 # The below code is for readthedocs. To have sphinx/readthedocs interact with
