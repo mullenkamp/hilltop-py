da5b124b11c3593d65776beabf45b06c94d99dcf
Mike Taves <mwtoews@gmail.com>
2021-09-20 14:54:20 +1200

    Drop Python 2 and related code

--- hilltoppy/util.py
+++ hilltoppy/util.py
@@ -83,20 +83,20 @@ def convert_site_names(names, rem_m=True):
     Function to convert water usage site names.
     """
 
-    names1 = names.str.replace('[:\.]', '/')
+    names1 = names.str.replace(r'[:\.]', '/')
 #    names1.loc[names1 == 'L35183/580-M1'] = 'L35/183/580-M1' What to do with this one?
 #    names1.loc[names1 == 'L370557-M1'] = 'L37/0557-M1'
 #    names1.loc[names1 == 'L370557-M72'] = 'L37/0557-M72'
 #    names1.loc[names1 == 'BENNETT K38/0190-M1'] = 'K38/0190-M1'
     names1 = names1.str.upper()
     if rem_m:
-        list_names1 = names1.str.findall('[A-Z]+\d+/\d+')
+        list_names1 = names1.str.findall(r'[A-Z]+\d+/\d+')
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
         names2[~names_len_bool] = np.nan
     else:
-        list_names1 = names1.str.findall('[A-Z]+\d+/\d+\s*-\s*M\d*')
+        list_names1 = names1.str.findall(r'[A-Z]+\d+/\d+\s*-\s*M\d*')
         names_len_bool = list_names1.apply(lambda x: len(x)) == 1
         names2 = names1.copy()
         names2[names_len_bool] = list_names1[names_len_bool].apply(lambda x: x[0])
